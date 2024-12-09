41d26d18704914501220a42cadfa64a5e2fbaa75
vienkkk <165811539+vienkkk@users.noreply.github.com>
2024-12-09 22:25:04 +0700

    Add files via upload

---
+++ Untitled3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+	int a=10;
+	int b=10;
+	printf("Nhap a = %d\n",a);
+	printf("Nhap b = %d\n",b);
+	int c=a+b;
+	int d=a-b;
+	int e=a*b;
+	int f=a/b;
+	printf("hieu = %d\n",d);
+	printf("tich = %d\n",e);
+	printf("thuong =%d\n",f); 
+	printf ("tong = %d\n",c);
+	return 0; 
+} 
